96c038b65d5f319b98d6ef65da6edaea6e69da57
danil <karatelanalov@yandex.ru>
2025-03-07 08:57:01 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Данил Дьяченко on 07.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
