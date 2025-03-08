8d3a4ef93c678372dd8b2c707add115616ac5362
danil <karatelanalov@yandex.ru>
2025-03-09 02:29:08 +0300

    add new field in model

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -17,11 +17,15 @@ struct LandmarkRow: View {
                 .frame(width: 50, height: 50)
             Text(landmark.name)
             Spacer()
+            
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+            }
         }
     }
 }
 
 #Preview("Turtle Rock") {
     LandmarkRow(landmark: landmarks[0])
-    LandmarkRow(landmark: landmarks[1])
 }
